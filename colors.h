a0c3fa94a0e3b3cad0d966f722b3d14e56de315e
sbbrain <penguin42.42@gmail.com>
2014-04-09 11:32:10 -0700

    Create colors.h

---
+++ colors.h
@@ -0,0 +1,17 @@
+#ifndef COLORS_H_INCLUDED
+#define COLORS_H_INCLUDED
+
+// Colors are defined here for code clearity. 
+// colors are a work in progress... I need More!
+
+uint32_t cBlack   = 0x000000;
+uint32_t cWhite   = 0xffffff;
+uint32_t cRed     = 0xff0000;
+uint32_t cGreen   = 0x00ff00;
+uint32_t cBlue    = 0x0000ff;
+uint32_t cYellow  = 0xffff00;
+uint32_t cCyan    = 0x00ffff;
+uint32_t cMagenta = 0xff00ff;
+uint32_t cOrange  = 0xff6600;
+
+#endif // COLORS_H_INCLUDED
